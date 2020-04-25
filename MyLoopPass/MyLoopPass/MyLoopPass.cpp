fbf191766ff4c6edb6ea64705338fa47bb4f645e
Ivan Korostelev <ivan23kor@gmail.com>
2020-04-25 11:43:14 -0600

    Rewrote into a FunctionPass, fixed codestyle, added BranchInst and printers to isPerfectNest

--- MyLoopPass/MyLoopPass/MyLoopPass.cpp
+++ MyLoopPass/MyLoopPass/MyLoopPass.cpp
@@ -2,14 +2,9 @@
 #include <string>
 #include <vector>
 
-#include "llvm/ADT/APInt.h"
-#include "llvm/ADT/SmallVector.h"
-#include "llvm/ADT/Statistic.h"
 #include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Analysis/LoopPass.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -22,68 +17,27 @@ namespace {
   // int NumNormalizedLoops = 0; // "Number of normalized loops");
   // int NumPerfectLoops = 0; // "Number of perfect loops");
 
-  inline std::string to_str(bool in) {
-    return in ? "yes" : "no";
+  inline std::string toStr(bool Value) {
+    return Value ? "yes" : "no";
   }
 
-  class MyLoopPass: public LoopPass {
+  class MyLoopPass: public FunctionPass {
   public:
     static char ID; // Pass identification, replacement for typeid
-    MyLoopPass() : LoopPass(ID) {}
+    // Deriving from FunctionPass for the correct order of loops
+    MyLoopPass() : FunctionPass(ID) {}
 
     void getAnalysisUsage(AnalysisUsage &AU) const override;
-    bool runOnLoop(Loop *loop, LPPassManager &LPM) override;
+    bool runOnFunction(Function &F) override;
+
+    // Could be of depth 1 (zero inner loops)
+    const BasicBlock *processLoopNest(const Loop *L) const;
+    int getLoopLevels(const Loop *L) const;
+    bool isPerfectNest(const Loop *L) const;
+    bool isNormalized(const Loop *L, ScalarEvolution &SE) const;
+    bool isNormalized2(const Loop *L, ScalarEvolution &SE) const;
+    bool isRectangularPolyhedron(const Loop *L, ScalarEvolution &SE) const;
   };
-
-  // Returns nestedness level of a loop, 1 for one loop
-  int getLoopLevels(const Loop *loop) {
-    int levels = 1;
-    int maxSubLooplevels = 0;
-    for (auto &subloop: loop->getSubLoops()) {
-        maxSubLooplevels = std::max(maxSubLooplevels, getLoopLevels(subloop));
-    }
-    return levels + maxSubLooplevels;
-  }
-
-  // Returns true if and only if every loop has one subloop and the only
-  bool isPerfectNest(const Loop *loop) {
-    // TODO: make simplify CFG work - merge or remove redundant blocks
-    if (loop->getBlocks().size() == 1) {
-      return true;
-    }
-    if (loop->getSubLoops().size() != 1) {
-      return false;
-    }
-    return isPerfectNest(*loop->begin());
-  }
-
-  // Returns true if and only if all the loops in this nest are normalized:
-  // Induction variable starts at 0 an increments by 1
-  bool isNormalized(const Loop *loop, ScalarEvolution &SE) {
-    if (!loop->getCanonicalInductionVariable()) {
-      return false;
-    }
-    // if (!loop->isCanonical(SE)) {
-    //   return false;
-    // }
-    for (auto &subloop: loop->getSubLoops()) {
-      return isNormalized(subloop, SE);
-    }
-    return true;
-  }
-
-  // Checks every level for the rectangularity of the induction variable,
-  // assumes the perfection of the nest had been checked
-  bool isRectangularPolyhedron(const Loop *loop, ScalarEvolution &SE) {
-    int nSubLoops = loop->getSubLoops().size();
-    if (!loop->getBounds(SE) || nSubLoops > 1) {
-      return false;
-    }
-    if (nSubLoops == 0) {
-      return true;
-    }
-    return isRectangularPolyhedron(*loop->begin(), SE);
-  }
 
   void MyLoopPass::getAnalysisUsage(AnalysisUsage &AU) const {
     AU.setPreservesAll();
@@ -91,26 +45,126 @@ namespace {
     AU.addRequired<ScalarEvolutionWrapperPass>();
   }
 
-  bool MyLoopPass::runOnLoop(Loop *loop, LPPassManager &LPM) {
-    ScalarEvolution &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+  bool MyLoopPass::runOnFunction(Function &F) {
+    LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
 
-    // Process loop nests only
-    if (loop->getParentLoop()) {
-        return false;
+    int count = 0;
+    // for (BasicBlock *BB = &*F.begin(), BE = &*F.end(); BB != BE; ++BB) {
+    for (auto BB = F.begin(), BE = F.end(); BB != BE; ++BB) {
+      if (LI.isLoopHeader(&*BB))
+        processLoopNest(LI.getLoopFor(&*BB));
+      errs() << count++ << ": " << &*BB << "\n";
+      // TODO: make processLoopNest skip to the end of the loop nest and return
+      // an iterator to an exit block
+      break;
     }
-
-    errs() << "Loop " << NumLoops++ << ": ";
-    errs() << "levels=" << getLoopLevels(loop);
-    errs() << ", perfect nest=" << to_str(isPerfectNest(loop));
-    errs() << ", normalized=" << to_str(isNormalized(loop, SE));
-    errs() << ", rectangular polyhedron="
-           << to_str(isRectangularPolyhedron(loop, SE));
-    errs() << "\n\n";
 
     return false;
   }
-
-  char MyLoopPass::ID = 0;
-  static RegisterPass<MyLoopPass> X("mylooppass",
-    "Loop info: depth, perfect and normalized", true, true);
 }
+
+const BasicBlock* MyLoopPass::processLoopNest(const Loop *L) const {
+  ScalarEvolution &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+  errs() << "Loop " << NumLoops++ << ": ";
+  errs() << "levels=" << getLoopLevels(L);
+  errs() << ", perfect nest=" << toStr(isPerfectNest(L));
+  errs() << ", normalized=" << toStr(isNormalized(L, SE));
+  errs() << ", normalized2=" << toStr(isNormalized2(L, SE));
+  errs() << ", rectangular polyhedron="
+         << toStr(isRectangularPolyhedron(L, SE));
+  errs() << "\n\n";
+  return *L->block_end();
+}
+
+// Returns nestedness level of a loop, 1 for one loop
+int MyLoopPass::getLoopLevels(const Loop *L) const {
+  int Levels = 1;
+  int MaxSubLoopLevels = 0;
+  for (auto &SubLoop: L->getSubLoops()) {
+      MaxSubLoopLevels = std::max(MaxSubLoopLevels, getLoopLevels(SubLoop));
+  }
+  return Levels + MaxSubLoopLevels;
+}
+
+// https://github.com/pmodels/bolt-llvm/blob/master/lib/Transforms/Scalar/LoopInterchange.cpp#L640
+bool MyLoopPass::isPerfectNest(const Loop *L) const {
+  switch (L->getSubLoops().size()) {
+  case 1:
+    break;
+  case 0:
+    return true;
+  default:
+    return false;
+  }
+
+  const Loop *SubLoop = L->getSubLoops()[0];
+
+  BasicBlock *LHeader = L->getHeader();
+  BasicBlock *SubLoopHeader = SubLoop->getHeader();
+  BasicBlock *SubLoopPreheader = SubLoop->getLoopPreheader();
+  BasicBlock *LLatch = L->getLoopLatch();
+
+  // Check for infinite loops?
+  BranchInst *LHeaderBI = dyn_cast<BranchInst>(LHeader->getTerminator());
+    errs() << "Header branch: ";
+  if (!LHeaderBI) {
+    errs() << "<empty>\n";
+    return false;
+  }
+  errs() << *LHeaderBI << "\n";
+
+  for (auto Succ: LHeaderBI->successors()) {
+    errs() << "\n";
+    errs() << "LHeader (" << LHeader << ")\n";
+    errs() << "SubLoopPreheader (" << SubLoopPreheader << ")\n";
+    errs() << "Succ (" << Succ << ")\n";
+    errs() << "SubLoopHeader (" << SubLoopHeader << ")\n";
+    errs() << "LLatch (" << LLatch << ")\n";
+  }
+
+  return true;
+}
+
+// Returns true if and only if all the loops in this nest are normalized:
+// Induction variable starts at 0 an increments by 1
+bool MyLoopPass::isNormalized(const Loop *L, ScalarEvolution &SE) const {
+  if (!L->getCanonicalInductionVariable()) {
+    return false;
+  }
+  // if (!loop->isCanonical(SE)) {
+  //   return false;
+  // }
+  for (auto &SubLoop: L->getSubLoops()) {
+    return isNormalized(SubLoop, SE);
+  }
+  return true;
+}
+
+bool MyLoopPass::isNormalized2(const Loop *L, ScalarEvolution &SE) const {
+  if (!L->isCanonical(SE)) {
+    return false;
+  }
+  for (auto &SubLoop: L->getSubLoops()) {
+    return isNormalized(SubLoop, SE);
+  }
+  return true;
+}
+
+// Checks every level for the rectangularity of the induction variable,
+// assumes the perfection of the nest had been checked
+bool MyLoopPass::isRectangularPolyhedron(const Loop *L, ScalarEvolution &SE) const {
+  int NumSubLoops = L->getSubLoops().size();
+  errs() << "<bounds=" << toStr(L->getBounds(SE).hasValue()) << "> ";
+  if (!L->getBounds(SE) || NumSubLoops > 1) {
+    return false;
+  }
+  if (NumSubLoops == 0) {
+    return true;
+  }
+  return isRectangularPolyhedron(*L->begin(), SE);
+}
+
+
+char MyLoopPass::ID = 0;
+static RegisterPass<MyLoopPass> X("mylooppass",
+  "Loop info: depth, perfect and normalized", true, true);
